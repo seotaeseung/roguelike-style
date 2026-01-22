b3fda885d3655db4af42f6c646372d20b21ec72f
gpry0001 <gpry0001@gmail.com>
2026-01-23 01:02:22 +0900

    타일맵 이용 맵설치
    맵을 제작할때 타일맵을 이용해 설치하도록 변경

--- Assets/DungeonGenerator.cs
+++ Assets/DungeonGenerator.cs
@@ -1,59 +1,87 @@
 using UnityEngine;
-using System.Collections.Generic;
-using UnityEditor.Experimental.GraphView;
+using UnityEngine.Tilemaps;
 
 
 public class DungeonGenerator : MonoBehaviour
 {
-    [Header("맵 설정")]
+    //시작할때 정하는 맵의 너비와 폭
     public int mapWidth = 100;
     public int mapHeight = 100;
     public int maxDepth = 3; // 몇 번이나 쪼갤지 (트리 깊이)
     private Node rootNode;
 
+    //맵을 기록하기 위한 이차원배열
     int[,] map;
+
+    //생성할 타일맵
+    public Tilemap tilemap;
+    public TileBase tileBase;
+
+    //맵 생성시 생성될 플레이어
+    public GameObject player;
     private void Start()
     {
+        //루트노드에 맵의 너비와 폭 시작 BottomLeft좌표를 보내줌
         rootNode = new Node(mapWidth, mapHeight, new Vector2Int(0, 0));
+        //기록할 이차원배열의 크기를 정함
         map = new int[mapWidth, mapHeight];
+        
 
         Divide(rootNode, 0);
         GenerateDungeon(rootNode);
         InstantiateMap();
+        SpawnPlayer(rootNode);
     }
+    
+
+    //노드와 깊이를 전달받는 나누는 함수
     public void Divide(Node node, int depth)
     {
+        //만약에 깊이가 최대 깊이보다 크면 종료
         if (depth > maxDepth) return;
+        //만약에 나눌 수 있다면
         if (node.Split())
         {
+            //왼쪽 오른쪽 노드와 깊이+1 을 전달해서 나눔
             Divide(node.LeftNode, depth + 1);
             Divide(node.RightNode, depth + 1);
         }
     }
+    //노드를 전달받는 던전생성 함수
     public void GenerateDungeon(Node node)
     {
+        //만약 자식이 없는노드라면
         if(node.LeftNode == null && node.RightNode == null)
         {
+            //방을 생성함
             node.GenerateRoom();
-            for(int i = 0; i < node.RoomWidth; i++)
+            //맵을 기록하는 이차원 배열에 기록 해당 좌표를 1로 변경함
+            
+            for (int i = 0; i < node.RoomWidth; i++)
             {
                 for(int j = 0; j < node.RoomHeight; j++)
                 {
+                    //방의 시작위치 기준으로 i,j만큼 떨어진 좌표계산
                     map[(int)node.roomBL.x + i, (int)node.roomBL.y + j] = 1;
                 }
             }
         }
-        else
+        else//자식이 있는 노드의 경우
         {
+            //왼쪽 오른쪽 노드중 존재하는 노드에 던전 생성
             if (node.LeftNode != null) GenerateDungeon(node.LeftNode);
             if (node.RightNode != null) GenerateDungeon(node.RightNode);
+            //그리고 해당노드에도 방을 생성
             node.GenerateRoom();
+            //자식노드의 중앙좌표를 가져와 저장
             Vector2 lCenter = node.LeftNode.Center;
             Vector2 rCenter = node.RightNode.Center;
 
+            //좌표에 따라 왼쪽노드가 오른쪽 노드보다 클수도 있으므로 min,max를 정해줌
             float xmin = Mathf.Min(lCenter.x, rCenter.x);
             float xmax = Mathf.Max(lCenter.x, rCenter.x);
 
+            //작은 값부터 큰값까지 길을 이차원 배열에1로 기록함
             for (int i = (int)xmin; i< (int)xmax; i++)
             {
                 map[i, (int)lCenter.y] = 1;
@@ -67,15 +95,49 @@ public class DungeonGenerator : MonoBehaviour
                 map[(int)rCenter.x, i] = 1;
             }
         }
+    } 
+
+    public void InstantiateMap()
+    {
+        // 맵 전체를 확인
+        for (int x = 0; x < mapWidth; x++)
+        {
+            for (int y = 0; y < mapHeight; y++)
+            {
+                //1인 곳에만 큐브를 생성합니다.
+                if (map[x, y] == 1)
+                {
+                    Vector3Int pos = new Vector3Int(x, y, 0);
+                    tilemap.SetTile(pos, tileBase);
+                }
+            }
+        }
     }
-    public void DrawDungeon(Node node)
+    //플레이어 생성 스크립트
+    public void SpawnPlayer(Node node)
+    {
+        //만약 왼쪽노드가 있다면 그곳으로 이동하게 하기 
+        if (node.LeftNode != null)
+        {
+            SpawnPlayer(node.LeftNode);
+        }
+        else //왼쪽 자식이 없는 노드라면 
+        {
+            Vector3Int spawnpos = new Vector3Int((int)node.Center.x,
+                (int)node.Center.y, 0);
+
+            Instantiate(player, tilemap.GetCellCenterWorld(spawnpos), Quaternion.identity);
+        }
+    }
+
+    /*public void DrawDungeon(Node node)
     {
         if (node == null) return;
 
         if (node.LeftNode == null && node.RightNode == null)
         {
             Gizmos.color = Color.blue;
-            
+
             Vector3 center = new Vector3(node.roomBL.x + (node.RoomWidth / 2f),
                                          node.roomBL.y + (node.RoomHeight / 2f), 0);
             Vector3 size = new Vector3(node.RoomWidth, node.RoomHeight, 0);
@@ -90,7 +152,7 @@ public class DungeonGenerator : MonoBehaviour
             Gizmos.DrawLine(node.LeftNode.Center, node.RightNode.Center);
         }
     }
-    /*public void OnDrawGizmos()
+    public void OnDrawGizmos()
     {
         DrawTree(rootNode);
         DrawDungeon(rootNode);
@@ -101,7 +163,7 @@ public class DungeonGenerator : MonoBehaviour
             {
                 for (int y = 0; y < mapHeight; y++)
                 {
-                    // 1(바닥)인 곳에만 빨간 큐브 그리기
+                    
                     if (map[x, y] == 1)
                     {
                         Gizmos.DrawCube(new Vector3(x, y, 0), Vector3.one);
@@ -109,7 +171,7 @@ public class DungeonGenerator : MonoBehaviour
                 }
             }
         }
-    }*/
+    }
     public void DrawTree(Node node)
     {
         if (node == null) return;
@@ -121,27 +183,5 @@ public class DungeonGenerator : MonoBehaviour
         Gizmos.DrawWireCube(center, size);
         DrawTree(node.LeftNode);
         DrawTree(node.RightNode);
-    }
-    [Header("프리팹 설정")]
-    public GameObject floorPrefab; // 여기에 큐브 프리팹 연결
-
-    public void InstantiateMap()
-    {
-        // 맵 전체를 훑습니다.
-        for (int x = 0; x < mapWidth; x++)
-        {
-            for (int y = 0; y < mapHeight; y++)
-            {
-                // 바닥(1)인 곳에만 큐브를 생성합니다.
-                if (map[x, y] == 1)
-                {
-                    // 위치는 (x, 0, y) 또는 (x, y, 0) 등 원하는 축으로 설정
-                    // 보통 3D 게임은 바닥이 X, Z 평면이므로 (x, 0, y)를 씁니다.
-                    Vector2 pos = new Vector2(x, y);
-
-                    Instantiate(floorPrefab, pos, Quaternion.identity, transform);
-                }
-            }
-        }
-    }
+    }*/
 }
--- Assets/Node.cs
+++ Assets/Node.cs
@@ -2,21 +2,24 @@ using UnityEngine;
 
 public class Node 
 {
+    //현재노드 너비와 폭
     public int Width;
     public int Height;
-    public Vector2Int TR;
-    public Vector2Int TL;
-    public Vector2Int BR;
+    //왼쪽아래 꼭짓점 좌표 저장
     public Vector2Int BL;
 
+    //부모노드와 자식노드
     public Node ParentNode;
     public Node LeftNode;
     public Node RightNode;
 
+    //만들어진 방의 너비와 폭
     public int RoomWidth;
     public int RoomHeight;
+    //만들어진 방의 왼쪽아래 꼭짓점 좌표 저장
     public Vector2 roomBL;
 
+    //방의 중앙좌표
     public Vector2 Center;
     public Node(int width, int height, Vector2Int bottomLeft)
     {
@@ -24,14 +27,15 @@ public class Node
         Height = height;
 
         BL = bottomLeft;
-        
     }
     public bool Split()
     {
+        //자식이 있다면 나누지 않음
         if(LeftNode != null || RightNode != null) return false;
-
+        //자식에게 전해줄 너비와 폭을 저장할 변수
         int cWidth;
         int cHeight;
+        //0이면 너비를 반으로, 1이면 폭을 반으로 나눔
         int num = Random.Range(0, 2);
         if (num == 0)
         {
@@ -43,9 +47,11 @@ public class Node
             cWidth = Width;
             cHeight = Height / 2;
         }
-
+        //나온 값을 왼쪽노드에 전달, 왼쪽노드이므로 BL은 부모의 BL과 같음
         LeftNode = new Node(cWidth, cHeight, BL);
 
+        //오른쪽 노드에 전해줄 BL값을 만드는 과정 위에서 너비와 폭 중 반으로 나눠진
+        //값에 따라 x y 좌표에 자식의 너비 or 폭을 더해줌
         Vector2Int newPos = this.BL;
         if (num == 0)
         {
@@ -55,26 +61,35 @@ public class Node
         {
             newPos.y += cHeight;
         }
+        //그리고 오른쪽 노드에 전달
         RightNode = new Node(cWidth, cHeight, newPos);
 
         return true;
     }
+
+    //방생성
     public void GenerateRoom()
     {
+        //자식이 없는노드인 경우
         if (LeftNode == null && RightNode == null)
         {
+            //방의 너비와 폭을 정함 위에서 만들어진 사각형 크기의 50%~ 90%
             RoomWidth = Random.Range(Width / 2, (int)(Width * 0.9f));
             RoomHeight = Random.Range(Height / 2, (int)(Height * 0.9f));
 
+            //만들어진 방의 BL좌표를 정하는 과정 
             int x = BL.x + Random.Range(0, Width - RoomWidth);
             int y = BL.y + Random.Range(0, Height - RoomHeight);
-
+            //나온 x y 값을 저장
             roomBL = new Vector2(x,y);
-
+            //방의 중앙 좌표를 정하는 공식
             Center = new Vector2(x + RoomWidth / 2, y + RoomHeight / 2);
         }
-        else
+        else //자식이 있는 노드의 경우(방이 없는 구역임)
         {
+            // 나는 방이 없으므로, 나중에 내 부모(할아버지)가 나와 길을 연결하려 할 때
+            // 연결할 좌표가 필요함.
+            // 따라서 내 자식 중 하나(왼쪽)의 좌표를 나의 대표 좌표로 삼음.
             Center = LeftNode.Center;
         }
 
